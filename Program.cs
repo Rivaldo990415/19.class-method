b5fc9d0dc7983257cd8f23dee5a09d950b956322
SBICZA01\A253638 <rivaldo.mkhola@standardbank.co.za>
2023-12-06 08:10:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.class_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(70, "John", 15);
+            Student student2 = new Student(40, "John", 10);
+
+            Console.WriteLine(student1.IsObese());
+            Console.WriteLine(student2.IsObese());
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.class_method
+{
+    internal class Student
+    {
+        public double weight;
+        public string name;
+        public int age;
+
+        public Student(double aWeight, string aName, int aAge)
+        {
+            this.weight = aWeight;
+            this.name = aName;
+            this.age = aAge;
+        }
+        //You now have one stop to test Obesity of your students and
+        public bool IsObese()
+        {
+            if(weight > 60)
+            {
+                return true;
+            }else return false;
+        }
+    }
+}
